e10c6dfc2c928d37d089bbc34f7c6def9ec8f9ef
Lok <loklok12614@gmail.com>
2022-03-23 15:50:57 -0500

    .gitignore

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(pdggyo4(4&3b=ban)k@6n1-d^mbv=5wja%_6tkl(2f@lox@6t'
+from .s_key import KEY
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
